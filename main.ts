f9bb269e366385ca7ad772c812b20adfd30c68af
Nora8900 <heer.vedanya@gmail.com>
2025-06-15 12:05:06 +0530

    Update main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onPinPressed(TouchPin.P0, function () {
+    game.addScore(randint(25, 100))
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.LoopingInBackground)
     basic.showNumber(randint(0, 100))
     if (Math.sqrt(0) < 25) {
         basic.showIcon(IconNames.Heart)
-        game.addScore(randint(25, 100))
     }
     game.gameOver()
 })
